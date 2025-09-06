3a81ea8bae12cb826cfa8ffe8d1d8450e13ff0c0
Vishal <sriram182719@gmail.com>
2025-09-06 23:48:57 +0530

    feat: update layout of Terminal section to reverse column order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,7 +116,7 @@ export default function Home() {
       <HeroSectionOne />
 
       {/* Terminal + Side Text Section */}
-      <div className="flex flex-col md:flex-row items-center gap-8 w-full mt-8">
+      <div className="flex flex-col md:flex-row-reverse items-center gap-8 w-full mt-8">
         {/* Terminal Section (Left) */}
         <div className="w-full md:w-2/3 md:pl-12">
           <Terminal>
